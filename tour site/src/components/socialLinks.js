15f8e3fed8b03f83ef1b9d76b277d59dd0b6ec06
Drakonz <124328116+DrakonZZZ@users.noreply.github.com>
2023-06-07 13:45:09 +0530

    Delete tour site/src directory

--- tour site/src/components/socialLinks.js
+++
@@ -1,8 +0,0 @@
-const socialLinks = [
-  { id: 0, icon: 'fab fa-facebook', URL: 'www.facebook.com' },
-  { id: 1, icon: 'fab fa-twitter', URL: 'www.twitter.com' },
-  { id: 2, icon: 'fab fa-reddit', URL: 'www.reddit.com' },
-  { id: 3, icon: 'fab fa-discord', URL: 'www.discord.com' },
-];
-
-export default socialLinks;
